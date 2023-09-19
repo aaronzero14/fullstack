036adcf889c7533f47f70be72b75b208c5bd8676
aaronzero14 <145498905+aaronzero14@users.noreply.github.com>
2023-09-19 13:34:34 -0600

    Add files via upload

---
+++ pages/Fullstack.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+//import styles from "../styles/Navbar.module.css"
+import Navbar from './Components/Navbar';
+
+
+
+export default function Fullstack() {
+ 
+
+
+  return (
+    <div className='Fullstack'>
+
+      <Navbar></Navbar>
+
+
+      <br />
+ 
+    </div>
+  );
+}
+
+
+ 
