6adb86edabc98d23a38bdeb842ff70f2b85b66d2
aaronzero14 <145498905+aaronzero14@users.noreply.github.com>
2023-09-24 17:10:23 -0600

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,8 @@ export default function Home() {
         <meta name="description" content="Aaron Fullstack Developer :))" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="hack.jpg" />
+
         
       </Head>
 
